35cd688ffbe4f17874d663ca95b5dffe9719ab55
John.Culviner <John.Culviner@target.com>
2015-02-12 14:35:38 -0600

    added instructions to run app

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-The code for the presentation "Introduction to Angular.js"
+Simple Angular SPA for "Introduction to Angular.js" presentation
+
+http://johnculviner.com/wp-content/uploads/2014/09/Intro-To-AngularJs-MDC.pptx
+
+Get it running:
+```
+git clone https://github.com/johnculviner/IntroToAngularJS.git
+```
+
+```
+npm install
+```
+
+```
+node server.js
+```
+
+App is now running at
+
+http://localhost:1337
--- public/app/app.js
+++ public/app/app.js
@@ -13,37 +13,4 @@ angular.module('faceFolio', ['ui.router', 'ngResource'])
                 templateUrl: 'app/person/person.html',
                 controller: 'person'
             });
-    })
-
-    .directive('fieldLocker', function() {
-        return {
-            scope: true,
-            transclude: true,
-            replace: true,
-            template: '<span>' +
-                        '<span ng-show="locked">{{value}}</span>' +
-                        '<span ng-show="!locked" ng-transclude="">textbox will go here</span>' +
-                        '<button ng-click="locked = false" ng-show="locked">Unlock</button>' +
-                      '</span>',
-            link: function(scope, ele, attrs) {
-
-                scope.locked = true;
-
-                scope.$watch(attrs.fieldLocker, function(val) {
-                    scope.value = val;
-                })
-
-
-                scope.unlock = function() {
-                    scope.locked = false;
-                };
-
-                scope.$on('saved', function() {
-                    scope.locked = true;
-                })
-
-            }
-        }
     });
-
-
